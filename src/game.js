a3f6811ed20ad25b8d7240888414dd0a18545b0b
Vallam Setti <venkat.vallamsetti@capgemini.com>
2021-11-06 20:31:14 +0530

    added new colsole log

--- src/game.js
+++ src/game.js
@@ -15,6 +15,7 @@ export default class Game {
     this.player = this.player === this.p1 ? this.p2 : this.p1
     this.sym = this.sym === 'X' ? 'O' : 'X'
     console.log('I am the next p');
+    console.log('Implementing new function for logs');
   }
 
   hasWinner () {
